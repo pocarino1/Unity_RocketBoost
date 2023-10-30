2fe209e9389ee10267e0aad3d79b9199f878af2f
pocarino1 <pocarino1@gmail.com>
2023-10-30 19:37:09 +0900

    RocketBoost Unity Project 생성

---
+++ Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    [SerializeField] private float BoostPower = 100.0f;
+    [SerializeField] private float RotationPower = 0.05f;
+
+    private Rigidbody RocketRigidbody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RocketRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            RocketRigidbody.AddRelativeForce(Vector3.up * BoostPower * Time.deltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            RocketRotation(RotationPower);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            RocketRotation(-RotationPower);
+        }
+    }
+
+    private void RocketRotation(float RotationValue)
+    {
+        RocketRigidbody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * RotationValue * Time.deltaTime);
+        RocketRigidbody.freezeRotation = false;
+    }
+}
