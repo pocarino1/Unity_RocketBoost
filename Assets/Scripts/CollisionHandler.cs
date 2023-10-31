b62ca24432ad9c51f0a5f9753d6ec2c0cd0ece86
pocarino1 <pocarino1@gmail.com>
2023-10-31 09:04:49 +0900

    게임 재시작 로직 추가

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    private void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Start":
+            {
+                Debug.Log("Rocket is Start Point!");
+            }
+            break;
+            case "Goal":
+            {
+                Debug.Log("Rocket is Goal Point!");
+            }
+            break;
+            case "Fuel":
+            {
+                Debug.Log("Pick Up Fuel!");
+                Destroy(other.gameObject);
+            }
+            break;
+            default:
+            {
+                Debug.Log("Crash! Rocket Destroy!");
+                Invoke("RestartGame", 1.0f);
+            }
+            break;
+        }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
